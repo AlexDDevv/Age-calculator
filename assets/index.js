f73d2deded34cc05398e8a98b56c5891b47ca0d3
Alexis <AlexDdevs@gmail.com>
2023-04-30 14:12:24 +0200

    age calculator

---
+++ assets/index.js
@@ -0,0 +1,157 @@
+const inputDay = document.getElementById("day");
+const inputMonth = document.getElementById("month");
+const inputYear = document.getElementById("year");
+const arrow = document.getElementById("arrowAge");
+
+/***** FONCTION VALIDATION *****/
+
+const displayErrorDay = (valid) => {
+  const labelDay = document.querySelector(".label-day");
+
+  if (!valid) {
+    inputDay.style.border = "1px solid hsl(0, 100%, 67%)";
+    errorDay.style.opacity = 1;
+    labelDay.style.color = "hsl(0, 100%, 67%)";
+  } else {
+    inputDay.style.border = "1px solid hsl(259, 100%, 65%)";
+    errorDay.style.opacity = 0;
+    labelDay.style.color = "hsl(0, 1%, 44%)";
+  }
+};
+const displayErrorMonth = (valid) => {
+  const labelMonth = document.querySelector(".label-month");
+
+  if (!valid) {
+    inputMonth.style.border = "1px solid hsl(0, 100%, 67%)";
+    errorMonth.style.opacity = 1;
+    labelMonth.style.color = "hsl(0, 100%, 67%)";
+  } else {
+    inputMonth.style.border = "1px solid hsl(259, 100%, 65%)";
+    errorMonth.style.opacity = 0;
+    labelMonth.style.color = "hsl(0, 1%, 44%)";
+  }
+};
+const displayErrorYear = (valid) => {
+  const labelYear = document.querySelector(".label-year");
+
+  if (!valid) {
+    inputYear.style.border = "1px solid hsl(0, 100%, 67%)";
+    errorYear.style.opacity = 1;
+    labelYear.style.color = "hsl(0, 100%, 67%)";
+  } else {
+    inputYear.style.border = "1px solid hsl(259, 100%, 65%)";
+    errorYear.style.opacity = 0;
+    labelYear.style.color = "hsl(0, 1%, 44%)";
+  }
+};
+
+/***** FONCTION VALIDATION *****/
+
+inputDay.addEventListener("input", () => {
+  const dayValue = document.getElementById("forDay");
+
+  if (inputDay.value.length == 0) {
+    displayErrorDay();
+    errorDay.textContent = "This field is required";
+  } else if (!inputDay.value.match(/^[0-9]+$/)) {
+    displayErrorDay();
+    errorDay.textContent = "Must be a valid day";
+  } else {
+    displayErrorDay(true);
+  }
+});
+
+inputMonth.addEventListener("input", () => {
+  const monthValue = document.getElementById("forMonth");
+
+  if (inputMonth.value.length == 0) {
+    displayErrorMonth();
+    errorMonth.textContent = "This field is required";
+  } else if (
+    inputMonth.value < 1 ||
+    inputMonth.value > 12 ||
+    !inputMonth.value.match(/^[0-9]+$/)
+  ) {
+    displayErrorMonth();
+    errorMonth.textContent = "Must be a valid month";
+  } else {
+    displayErrorMonth(true);
+  }
+});
+
+inputYear.addEventListener("input", () => {
+  const yearValue = document.getElementById("forYear");
+
+  if (inputYear.value.length == 0) {
+    displayErrorYear();
+    errorYear.textContent = "This field is required";
+  } else if (!inputYear.value.match(/^[0-9]+$/)) {
+    displayErrorYear();
+    errorYear.textContent = "Must be a valid year";
+  } else if (inputYear.value > 2023) {
+    displayErrorYear();
+    errorYear.textContent = "Must be in the past";
+  } else {
+    displayErrorYear(true);
+  }
+});
+
+/****** FONCTION CALCULER AGE *****/
+
+/*function calculerAge() {
+  const now = new Date();
+  const dateNaissance = new Date(
+    inputYear.value,
+    inputMonth.value - 1,
+    inputDay.value
+  );
+
+  // Calcul de l'âge en jours
+  const diffJours = Math.floor((now - dateNaissance) / (1000 * 60 * 60 * 24));
+  forDay.textContent = diffJours;
+
+  // Calcul de l'âge en mois et années
+  const ageMois =
+    now.getMonth() -
+    dateNaissance.getMonth() +
+    12 * (now.getFullYear() - dateNaissance.getFullYear());
+  const ageAnnees = Math.floor(ageMois / 12);
+  forMonth.textContent = ageMois;
+  forYear.textContent = ageAnnees;
+}*/
+
+function calculateAge() {
+  const now = new Date();
+  const birthdate = new Date(
+    inputYear.value,
+    inputMonth.value - 1,
+    inputDay.value
+  );
+
+  let ageYears = now.getFullYear() - birthdate.getFullYear();
+  let ageMonths = now.getMonth() - birthdate.getMonth();
+  let ageDays = now.getDate() - birthdate.getDate();
+
+  if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
+    ageYears--;
+    ageMonths += 12;
+  }
+  if (ageDays < 0) {
+    ageDays += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+    ageMonths--;
+  }
+
+  ageYears = Math.max(ageYears, 0);
+  ageMonths = Math.max(ageMonths, 0);
+  ageDays = Math.max(ageDays, 0);
+
+  forYear.textContent = ageYears;
+  forMonth.textContent = ageMonths;
+  forDay.textContent = ageDays;
+}
+
+/****** FONCTION CALCULER AGE *****/
+
+arrow.addEventListener("click", () => {
+  calculateAge();
+});
